560057dfd13abe89bf0cd0cc8186323368be8214
starshyp <54755762+starshyp@users.noreply.github.com>
2020-06-09 12:59:54 -0400

    initial commit

---
+++ franklin/includes/footer.php
@@ -0,0 +1,36 @@
+      <div id="footer" class="cf">
+
+        <div class="column three">
+          <strong>Phone</strong>
+          808.529.3819
+        </div>
+
+        <div class="column three">
+          <strong>Location</strong>
+          123 Kapiolani Boul.
+          Honolulu, HI
+        </div>
+
+        <div class="column three last">
+
+          <strong>Hours</strong>
+          <em>Tuesday - Thursday</em><br>
+          1:00pm - 9:00pm<br><br>
+
+          <em>Friday - Saturday</em><br>
+          4:00pm - 11:00pm<br><br>
+
+          <em>Sunday - Monday</em><br>
+          Closed<br><br>
+
+          <?php include("includes/store-hours.php"); ?>
+
+        </div>
+      </div>
+      <small>&copy;<?php echo date("Y");?> <?php echo $companyName; ?></small>
+    </div>
+
+  </div>
+
+</body>
+</html>
---
+++ franklin/includes/header.php
@@ -0,0 +1,23 @@
+<?php
+  $companyName = "Franklin's Fine Dining";
+  include("includes/arrays.php");
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo TITLE; ?></title>
+  <link href="./assets/styles.css" rel="stylesheet">
+
+</head>
+
+<body id="final-example">
+  <div class="wrapper">
+    <div id="banner">
+      <a href="/" title="Return to Home">
+        <img src="img/banner.png" alt="Franklin's Find Dining">
+      </a>
+    </div>
+    <div id="nav">
+        <?php include("includes/nav.php"); ?>
+    </div>
+    <div class="content">
